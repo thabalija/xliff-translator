a377ccd9b9aa524687304c4508751f353de3034d
Tomislav <tomislav.habalija@gmail.com>
2018-01-12 19:19:48 +0100

    Upload and download files enabled

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -9,6 +9,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FileUploadComponent implements OnInit {
 
   fileInput: FormGroup;
+  uploadedFile;
+  finalFile;
+  uploadedFileInfo = {
+    'targetLang': '',
+    'sourceLang': '',
+    'xliffVersion': '',
+    'datatype': '',
+    'original': ''
+  };
+  translationUnits = [];
 
   constructor(
     private fb: FormBuilder
@@ -27,37 +37,81 @@ export class FileUploadComponent implements OnInit {
 
   openFile(event) {
     const input = event.target;
-    for (let index = 0; index < input.files.length; index++) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        // this 'text' is the content of the file
-        const text = reader.result;
+    const file = input.files[0];
 
-        const parser = new DOMParser();
-        const htmlDoc = parser.parseFromString(text, 'text/html');
+    const reader = new FileReader();
 
-        const elements = htmlDoc.getElementsByTagName('trans-unit');
+    reader.onload = () => {
 
-        const arr = Array.from(elements);
+      // this 'text' is the content of the file
+      const text = reader.result;
 
-        const myArray = []; // empty Array
-        for ( let i = 0; i < elements.length; i++) {
-            const self = {
-              id: elements[i].getAttribute('id'),
-              source: elements[i].querySelector('source').innerHTML,
-              target: elements[i].querySelector('target').innerHTML,
-              targetState: elements[i].querySelector('target').getAttribute('state'),
-              note: elements[i].getElementsByTagName('note')
-            };
-            myArray.push(self);
-        }
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(text, 'application/xhtml+xml');
+      
+      this.uploadedFile = htmlDoc;
 
-        console.log(myArray);
-      };
-      reader.readAsText(input.files[index]);
-    }
+      console.log(htmlDoc);
+
+      const elements = htmlDoc.getElementsByTagName('trans-unit');
+      const arr = Array.from(elements);
+
+      const fileElement = htmlDoc.getElementsByTagName('file')[0];
+      this.uploadedFileInfo.sourceLang = fileElement.getAttribute('source-language');
+      this.uploadedFileInfo.targetLang = fileElement.getAttribute('target-language');
+      this.uploadedFileInfo.datatype = fileElement.getAttribute('datatype');
+      this.uploadedFileInfo.original = fileElement.getAttribute('original');
+      const xliffElement = htmlDoc.getElementsByTagName('xliff')[0];
+      this.uploadedFileInfo.xliffVersion = xliffElement.getAttribute('version');
+
+      arr.forEach( (el) => {
+        const self = {
+          id: el.getAttribute('id'),
+          source: el.querySelector('source').innerHTML,
+          target: el.querySelector('target').innerHTML,
+          targetState: el.querySelector('target').getAttribute('state'),
+          note: el.getElementsByTagName('note')
+        };
+        this.translationUnits.push(self);
+      });
+
+      htmlDoc
+        .getElementById('9a91783e9c0f790ed49edae730f9156070ed9dd5')
+        .getElementsByTagName('target')[0] .innerHTML = 'dickbutt';
+
+
+      const stringer = new XMLSerializer();
+
+      this.finalFile = stringer.serializeToString(htmlDoc);
+
+    };
+
+    reader.readAsText(file);
+
   }
 
+  downloadFileCall() {
+
+    this.translationUnits.forEach( (transUnit) => {
+      this.uploadedFile.getElementById(transUnit.id).getElementsByTagName('target')[0] .innerHTML = transUnit.target;
+    });
+
+    const stringer = new XMLSerializer();
+
+    this.finalFile = stringer.serializeToString(this.uploadedFile);
+
+    this.downloadFile(this.finalFile, `myName.${this.uploadedFileInfo.targetLang}.xlf`);
+  }
+
+  downloadFile(file, fileName) {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(file));
+    element.setAttribute('download', fileName);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
 
 }
 
@@ -66,5 +120,5 @@ export interface TransUnit {
   source: string;
   target?: string;
   targetState?: string;
-  note?: string;
+  note?: any;
 }
