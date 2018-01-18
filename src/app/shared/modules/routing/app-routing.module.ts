2416fec7ca4129a300b06f11e0bc09b52ac75233
Tomislav <tomislav.habalija@gmail.com>
2018-01-18 22:42:22 +0100

    Removed unused routes

--- src/app/shared/modules/routing/app-routing.module.ts
+++ src/app/shared/modules/routing/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Components
 import { LayoutComponent } from './../../../components/layout/layout.component';
+import { FileUploadComponent } from './../../../components/file-upload/file-upload.component';
+import { TranslationsComponent } from './../../../components/translations/translations.component';
+import { EditorComponent } from './../../../components/editor/editor.component';
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent, pathMatch: 'full'}
+  { path: '', component: LayoutComponent},
+  { path: '*', redirectTo: '', pathMatch: 'full'}
 ];
 
 @NgModule({
