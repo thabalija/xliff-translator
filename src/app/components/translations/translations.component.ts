6eea1808538c545f0f8cfb0ae7e5880894dca1ba
Tomislav <tomislav.habalija@gmail.com>
2018-01-20 20:13:46 +0100

    Refactored translations

--- src/app/components/translations/translations.component.ts
+++ src/app/components/translations/translations.component.ts
@@ -6,6 +6,9 @@ import { FileInfo } from './../../shared/interfaces/file-info.interface';
 import { TranslationUnit } from './../../shared/interfaces/translation-unit.interface';
 import { FileUploadService } from './../../services/file-upload.service';
 import { FileDownloadService } from './../../services/file-download.service';
+import { AddTranslationService } from './../../services/add-translation.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { AddTranslationDialogComponent } from './../../shared/modules/shared/add-translation-dialog/add-translation-dialog.component';
 
 @Component({
   selector: 'app-translations',
@@ -14,7 +17,7 @@ import { FileDownloadService } from './../../services/file-download.service';
 })
 export class TranslationsComponent implements OnInit {
 
-  fileInfo: FileInfo;
+  baseFileInfo: FileInfo;
   translationsList: FileInfo[] = [];
   displayedColumns = ['language', 'fileName', 'status', 'translate', 'download'];
   dataSource = new MatTableDataSource<FileInfo>();
@@ -22,24 +25,53 @@ export class TranslationsComponent implements OnInit {
   constructor(
     public _router: Router,
     private _fileUploadService: FileUploadService,
-    private _fileDownloadService: FileDownloadService
+    private _fileDownloadService: FileDownloadService,
+    private _addTranslationService: AddTranslationService,
+    private _dialog: MatDialog
   ) { }
 
   ngOnInit() {
-    this.loadFileInfo();
+    this.loadBaseFileInfo();
     this.loadTranslations();
-    this.dataSource.data = this.translationsList;
+  }
+
+  // load file info
+  loadBaseFileInfo() {
+    this.baseFileInfo = this._fileUploadService.getFileInfo();
   }
 
   // create translations
   loadTranslations() {
-    console.log(this.fileInfo);
-    this.translationsList = [this.fileInfo];
+    this.translationsList = this._addTranslationService.getTranslationList();
+    this.dataSource.data = this.translationsList;
+    console.log(this.translationsList);
   }
 
-  // load file info
-  loadFileInfo() {
-    this.fileInfo = this._fileUploadService.getFileInfo();
+  // add new translation
+  addTranslation() {
+    this._router.navigate([`/add-translation`]);
+  }
+
+  // open add translation dialog
+  openAddTranslationDialog() {
+    const dialogRef = this._dialog.open(AddTranslationDialogComponent, {
+      width: '500px',
+      data: {
+        name: '',
+        targetLang: ``
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.createTranslation(result.name, result.targetLang);
+      }
+    });
+  }
+
+  // create new translation
+  createTranslation(fileName: string, targetLang: string) {
+    this._addTranslationService.addTranslation(fileName, targetLang);
+    this.loadTranslations();
   }
 
   // open selected translation
