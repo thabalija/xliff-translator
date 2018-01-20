33f7a78fc3348c3e42fbaa4dd67b0afba9f42440
Tomislav <tomislav.habalija@gmail.com>
2018-01-20 20:12:40 +0100

    Refactored modal dialog

--- src/app/shared/modules/shared/add-translation-dialog/add-translation-dialog.component.ts
+++ src/app/shared/modules/shared/add-translation-dialog/add-translation-dialog.component.ts
@@ -2,14 +2,14 @@ import { Component, Injectable, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
-    templateUrl: './settings-dialog.component.html'
+    templateUrl: './add-translation-dialog.component.html'
 })
-export class SettingsDialogComponent {
+export class AddTranslationDialogComponent {
 
     localeArray = [];
 
     constructor(
-        public dialogRef: MatDialogRef<SettingsDialogComponent>,
+        public dialogRef: MatDialogRef<AddTranslationDialogComponent>,
         @Inject(MAT_DIALOG_DATA) public data: any) {
         this.localeArray = data.localeArray;
     }
