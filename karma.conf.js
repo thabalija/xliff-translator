ce050fc4451eae018290ff5da679edbce461ad22
Tomislav Habalija <tomislav.habalija@infinum.hr>
2019-12-30 13:24:50 +0100

    Removed not needed browsers

--- karma.conf.js
+++ karma.conf.js
@@ -24,10 +24,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeNoSandbox'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers:{
-      ChromeNoSandbox:{
-        base: 'Chrome',
+      ChromeHeadlessNoSandbox:{
+        base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     },
